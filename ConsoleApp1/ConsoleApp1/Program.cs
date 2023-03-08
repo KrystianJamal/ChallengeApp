5e309a3532fb7effcd39c6b9b329cca2a62fce9d
Krystian Jamal <juszczykrystian@gmail.com>
2023-03-08 11:47:24 +0100

    Zadanie domowe dzien 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// zadanie domowe dzien 4
+
+string imię = "Krystian";
+bool facet = false;
+int wiek = 28;
+
+if (facet == true)
+{
+    if (imię == "Krystian" && wiek == 28)
+    {
+        Console.WriteLine("Krystian, lat 28");
+    }
+    else if (imię != "Krystian" && wiek < 28)
+    {
+        Console.WriteLine("Facet poniżej 30 lat");
+    }
+}
+else // mężczyzna
+{
+    if (wiek < 19)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else if (imię == "Kaśka")
+    {
+        Console.WriteLine("Czy mężczyzna może miec na imię Kaśka?");
+    }
+}
+
